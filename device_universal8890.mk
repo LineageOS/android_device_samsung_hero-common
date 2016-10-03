29246d18cc606a94e7d647b3ef3bdc0523e4616c
Christopher N. Hesse <raymanfx@gmail.com>
2016-10-03 15:00:33 +0200

    universal8890: Enable freeform windows
    * Cherry-picked from android_device_samsung_gts2-common
    
    Change-Id: I039afd04d17f87f2b3070d915c8243210f075df4
    
    Signed-off-by: Jesse Chan <jc@linux.com>

--- device_universal8890.mk
+++ device_universal8890.mk
@@ -66,6 +66,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:system/etc/permissions/android.software.freeform_window_management.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/android.software.sip.xml:system/etc/permissions/android.software.sip.xml \
