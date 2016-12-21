37db8cea586d3d3f9d94f68941452859ee4b7521
Fevax <fer.vonarx@gmail.com>
2016-12-21 14:19:17 -0300

    update tree for cm-14.1

--- cm.mk
+++ cm.mk
@@ -5,8 +5,6 @@ PRODUCT_RELEASE_NAME := hero2ltexx
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 # Inherit device configuration
 $(call inherit-product, device/samsung/hero2ltexx/device_hero2ltexx.mk)
 
