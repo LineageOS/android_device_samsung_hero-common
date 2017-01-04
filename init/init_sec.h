CyanogenMod/android_device_samsung_zero-common

libinit_sec: Fix bootloader matching
* don't explicitly match an obvious substring
* Use std::find() to check for substring

Change-Id: I219273fc9533c04867c59c7b526bfee5b8c1c9a7
Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

init/init_sec.h
@@ -27,17 +27,17 @@ enum device_variant {
 
 device_variant match(std::string bl)
 {
-    if (bl == "G920S") {
+    if (bl.find("G920S") != std::string::npos) {
         return G920S;
-    } else if (bl == "G920K") {
+    } else if (bl.find("G920K") != std::string::npos) {
         return G920K;
-    } else if (bl == "G920L") {
+    } else if (bl.find("G920L") != std::string::npos) {
         return G920L;
-    } else if (bl == "G920P") {
+    } else if (bl.find("G920P") != std::string::npos) {
         return G920P;
-    } else if (bl == "G920I") {
+    } else if (bl.find("G920I") != std::string::npos) {
         return G920I;
-    } else if (bl == "G920F") {
+    } else if (bl.find("G920F") != std::string::npos) {
         return G920F;
     } else {
         return UNKNOWN;

LineageOS/android_device_samsung_hero-common

Signed-off-by: Jesse Chan <jc@linux.com>

init/init_sec.h
@@ -13,23 +13,23 @@ enum device_variant {
 
 device_variant match(std::string bl)
 {
-    if (bl == "G930F") {
+    if (bl.find("G930F") != std::string::npos) {
         return G930F;
-    } else if (bl == "G930W8") {
+    } else if (bl.find("G930W8") != std::string::npos) {
         return G930W8;
-    } else if (bl == "G930S") {
+    } else if (bl.find("G930S") != std::string::npos) {
         return G930S;
-    } else if (bl == "G930K") {
+    } else if (bl.find("G930K") != std::string::npos) {
         return G930K;
-    } else if (bl == "G930L") {
+    } else if (bl.find("G930L") != std::string::npos) {
         return G930L;
-    } else if (bl == "G935S") {
+    } else if (bl.find("G935S") != std::string::npos) {
         return G935S;
-    } else if (bl == "G935K") {
+    } else if (bl.find("G935K") != std::string::npos) {
         return G935K;
-    } else if (bl == "G935L") {
+    } else if (bl.find("G935L") != std::string::npos) {
         return G935L;
-    } else if (bl == "G935W8") {
+    } else if (bl.find("G935W8") != std::string::npos) {
         return G935W8;
     } else {
         return G935F;
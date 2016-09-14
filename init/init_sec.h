025339fbc1f3e02388d689eefe1de667ec354006
Brandon McAnsh <brandon.mcansh@gmail.com>
2016-09-13 23:49:11 -0400

    libinit_sec: Fix bootloader matching
    * don't explicitly match an obvious substring
    * Use std::find() to check for substring
    
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>
    Signed-off-by: Jesse Chan <jc@linux.com>

--- init/init_sec.h
+++ init/init_sec.h
@@ -13,23 +13,23 @@ enum device_variant {
 
 device_variant match(std::string bl)
 {
-    if (bl == "G930F") {
+    if (bl.find("G930F") != std::string::npos) {
         return G930F;
-    } else if (bl == "G930W8") {
+    } else if (bl.find("G930W8") != std::string::npos) {
         return G930W8;
-    } else if (bl == "G930S") {
+    } else if (bl.find("G930S") != std::string::npos) {
         return G930S;
-    } else if (bl == "G930K") {
+    } else if (bl.find("G930K") != std::string::npos) {
         return G930K;
-    } else if (bl == "G930L") {
+    } else if (bl.find("G930L") != std::string::npos) {
         return G930L;
-    } else if (bl == "G935S") {
+    } else if (bl.find("G935S") != std::string::npos) {
         return G935S;
-    } else if (bl == "G935K") {
+    } else if (bl.find("G935K") != std::string::npos) {
         return G935K;
-    } else if (bl == "G935L") {
+    } else if (bl.find("G935L") != std::string::npos) {
         return G935L;
-    } else if (bl == "G935W8") {
+    } else if (bl.find("G935W8") != std::string::npos) {
         return G935W8;
     } else {
         return G935F;
