954389f7f11e7ff4015860107a134b68ccd7c9e5
Jesse Chan <jc@linux.com>
2017-01-20 19:34:57 +0800

    hero: ramdisk: clean up
    * Remove unused services
    
    Change-Id: I3253699215bef9922e460fafa0c61d069f610f72

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -48,14 +48,6 @@ LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE		:= init.carrier.rc
-LOCAL_MODULE_TAGS	:= optional eng
-LOCAL_MODULE_CLASS	:= BOOT
-LOCAL_SRC_FILES		:= init.carrier.rc
-LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.gps.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= BOOT
