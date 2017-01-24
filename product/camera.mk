2e2909c13e314034d365c9464c5e06c02ffa00f1
Fevax <fer.vonarx@gmail.com>
2017-01-24 23:05:45 +0800

    Revert "Revert "universal8890: Switch back to Camera2""
    This reverts commit 1aa5b4c8bbcf2f0dd6470761fea122c0c3a4aa58.
    
    Change-Id: Iadeba829a6a0773fcacee39669a9a46f9358fbd1
    Signed-off-by: Jesse Chan <jc@linux.com>

--- product/camera.mk
+++ product/camera.mk
@@ -1,7 +1,3 @@
-# Packages
-PRODUCT_PACKAGES += \
-	Snap
-
 # Permissions
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
