1dcb5a88bfc57e9f4aeccfa70259e058d86695b8
Jesse Chan <jc@linux.com>
2017-01-25 07:21:58 +0100

    Revert "universal8890: use custom kernel toolchain"
    This reverts commit 3c4b0a90a370c17df1a88a2fbdf85c7ad0453b9a.
    
    Change-Id: Ifcf7a559bbdd5ad9195c9adb8bb6df6f78e63691

--- board/kernel.mk
+++ board/kernel.mk
@@ -5,8 +5,3 @@ TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 2048
 TARGET_USES_UNCOMPRESSED_KERNEL := true
-
-# Kernel Toolchain
-# - Default toolchain has some compatibility problems with Exynos Kernel.
-KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-7.0/bin
