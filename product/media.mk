dd717f2d841a4e62e77b721f181639cbfab84f0b
Fevax <fer.vonarx@gmail.com>
2017-02-02 21:42:55 +0800

    Revert "universal8890: Allow OMX libraries to load again"
    This reverts commit bc2649f9e1f9ebbc9ed9ea96c1686daceddb9d95.
    
    * commit bc2649f9e1f9ebbc9ed9ea96c1686daceddb9d95 is not really functional
      as we are using prebuilt OMX libs from stock N firmware.
    
    Change-Id: I1c9a55357f902c340a39c382b7e7a6f34ee6d187
    Signed-off-by: Jesse Chan <jc@linux.com>

--- product/media.mk
+++ product/media.mk
@@ -6,8 +6,3 @@ PRODUCT_COPY_FILES += \
 	frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
 	$(DEVICE_PATH)/configs/media/media_codecs.xml:system/etc/media_codecs.xml \
 	$(DEVICE_PATH)/configs/media/media_profiles.xml:system/etc/media_profiles.xml
-
-# Properties
-PRODUCT_PROPERTY_OVERRIDES += \
-	media.stagefright.legacyencoder=1 \
-	media.stagefright.less-secure=1
