4625bb499a6381354c87dc39fb3ca1d8c5dfced1
Jesse Chan <jc@linux.com>
2017-02-02 22:11:02 +0800

    hero: change the behavior of DEX Pre-Optimization
    * DEX Pre-Optimization is now disabled on eng builds.
      It is still a bit aggressive than mainstream(only PREOPT on user builds).
      But users want faster boot speed and Samsung devices have
      extra-large system partition so I think it should be OK.
    
    * Now only boot images are pre-optimized. It should reduce size of package.
    
    Change-Id: I43e58c40f32e2fe17616b4376762d93ef063ec44

---
+++ board/dexopt.mk
@@ -0,0 +1,8 @@
+ifeq ($(HOST_OS),linux)
+    ifneq ($(TARGET_BUILD_VARIANT),eng)
+        ifeq ($(WITH_DEXPREOPT),)
+            WITH_DEXPREOPT := true
+            WITH_DEXPREOPT_BOOT_IMG_ONLY ?= true
+        endif
+    endif
+endif
