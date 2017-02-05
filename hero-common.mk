6b00baac0d270d65e6f63797803deb29a7d25661
Jesse Chan <jc@linux.com>
2017-02-05 19:30:09 +0800

    hero: compile fingerprint components
    Change-Id: Ic62e32cb3610e18070692d5ff6f5667858b50aa5

--- hero-common.mk
+++ hero-common.mk
@@ -34,7 +34,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml \
-	frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml \
 	frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
 	frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
 	frameworks/native/data/etc/android.software.freeform_window_management.xml:system/etc/permissions/android.software.freeform_window_management.xml \
