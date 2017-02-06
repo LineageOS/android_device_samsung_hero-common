8abfaba4b08b11cc8241dc1e870230cedf286088
Jesse Chan <jc@linux.com>
2017-02-06 10:36:25 +0800

    hero: configure audio HAL constants
    Change-Id: Ie20fd61b415400eca6cd6387162a093597a64398

---
+++ include/samsung_audio.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2017 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef SAMSUNG_AUDIO_H
+#define SAMSUNG_AUDIO_H
+
+#define MIXER_CARD 0
+#define SOUND_CARD 0
+
+/* Playback */
+#define SOUND_DEEP_BUFFER_DEVICE 3
+#define SOUND_PLAYBACK_DEVICE 6
+#define SOUND_PLAYBACK_SCO_DEVICE 2
+
+/* Capture */
+#define SOUND_CAPTURE_DEVICE 0
+#define SOUND_CAPTURE_SCO_DEVICE 2
+
+/* DSP offload */
+#define SOUND_COMPRESS_OFFLOAD_DEVICE 11
+
+/* Voice calls */
+#define PCM_DEVICE_VOICE 1
+
+/* Unusupported
+#define SOUND_CAPTURE_LOOPBACK_AEC_DEVICE 1
+#define SOUND_CAPTURE_HOTWORD_DEVICE 0
+*/
+
+
+#endif // SAMSUNG_AUDIO_H
