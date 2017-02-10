9542c10b0d958bd6fe8b7ce86e249f2e0470c4d7
Jesse Chan <jc@lineageos.org>
2017-02-10 21:07:23 +0800

    hero: add common properties
    Change-Id: Ie1b5e5e58bf4a6b474e91dabc05c0ae59300a298

--- board/rec.mk
+++ board/rec.mk
@@ -5,3 +5,4 @@ BOARD_HAS_NO_SELECT_BUTTON := true
 TARGET_RECOVERY_PIXEL_FORMAT := "ABGR_8888"
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun/file
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
+TARGET_RECOVERY_FSTAB := device/samsung/hero-common/ramdisk/fstab.samsungexynos8890
