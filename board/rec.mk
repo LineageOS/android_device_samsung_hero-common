f5887bb0ebc02c71dd9bdef2d1827255f1c9dbfc
Fevax <fer.vonarx@gmail.com>
2017-02-10 21:24:55 +0800

    hero: correct TARGET_USE_CUSTOM_LUN_FILE_PATH
    Change-Id: Ica112158729abee94be8eaca3dfeac9366f0ccd6

--- board/rec.mk
+++ board/rec.mk
@@ -3,6 +3,6 @@ BOARD_RECOVERY_SWIPE := true
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
 BOARD_HAS_NO_SELECT_BUTTON := true
 TARGET_RECOVERY_PIXEL_FORMAT := "ABGR_8888"
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun/file
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/15400000.usb/15400000.dwc3/gadget/lun%d/file"
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
 TARGET_RECOVERY_FSTAB := device/samsung/hero-common/ramdisk/fstab.samsungexynos8890
