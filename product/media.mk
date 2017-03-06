0bdffa7bba4993c84f0ac0ac54c34258f1aa7b1a
Jesse Chan <jc@lineageos.org>
2017-03-06 18:14:43 +0800

    hero: media: add SOMX codecs
    Change-Id: I6fc946672838d1185422018e4f63dd2433b13168

--- product/media.mk
+++ product/media.mk
@@ -1,8 +1,14 @@
 DEVICE_PATH := device/samsung/hero-common
 
+# Configs
 PRODUCT_COPY_FILES += \
 	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
 	$(DEVICE_PATH)/configs/media/media_codecs.xml:system/etc/media_codecs.xml \
-	$(DEVICE_PATH)/configs/media/media_profiles.xml:system/etc/media_profiles.xml
+	$(DEVICE_PATH)/configs/media/media_profiles.xml:system/etc/media_profiles.xml \
+	$(DEVICE_PATH)/configs/media/somxreg.conf:system/etc/somxreg.conf
+
+# Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+	media.sf.omx-plugin=libffmpeg_omx.so,libsomxcore.so
