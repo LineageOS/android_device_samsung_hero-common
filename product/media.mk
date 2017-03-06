95b848cd762e58e367190e2eb838bdf5a42aca0e
Jesse Chan <jc@lineageos.org>
2017-03-06 18:22:11 +0800

    hero: media: add codecs performance config
    Change-Id: Ib44d01fb734b878cac499761280c510ac85e0510

--- product/media.mk
+++ product/media.mk
@@ -6,6 +6,7 @@ PRODUCT_COPY_FILES += \
 	frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
 	$(DEVICE_PATH)/configs/media/media_codecs.xml:system/etc/media_codecs.xml \
+	$(DEVICE_PATH)/configs/media/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
 	$(DEVICE_PATH)/configs/media/media_profiles.xml:system/etc/media_profiles.xml \
 	$(DEVICE_PATH)/configs/media/somxreg.conf:system/etc/somxreg.conf
 
