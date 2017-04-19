6a6159a03f7a8624c92949a1b9a0c02c37e38541
Gabriele M <moto.falcon.git@gmail.com>
2017-04-18 20:13:40 -0500

    hlteRIL: Handle RIL_UNSOL_NITZ_TIME_RECEIVED
    * This fixes auto time zone
    * This is related to how it was handled in the
      old SamsungQualcommRIL class (deprecated) but
      the Parcel is fixed rather than handling it
      directly in the RIL subclass
    * This is done so that after we handle it,
      it's handled by processUnsolicited in the
      super class, which handles this UNSOL
    * The extra "garbage" that is sent in the
      RIL_UNSOL_NITZ_TIME_RECEIVED parcel seems
      to be country MCC code for whatever reason
    
    Change-Id: I70982873e6304d81794bdd03eb8d9a1c1d3f3a55

--- ril/telephony/java/com/android/internal/telephony/SlteRIL.java
+++ ril/telephony/java/com/android/internal/telephony/SlteRIL.java
@@ -313,6 +313,24 @@ public class SlteRIL extends RIL {
         return response;
     }
 
+    private void
+    fixNitz(Parcel p) {
+        int dataPosition = p.dataPosition();
+        String nitz = p.readString();
+        long nitzReceiveTime = p.readLong();
+
+        String[] nitzParts = nitz.split(",");
+        if (nitzParts.length >= 4) {
+            // 0=date, 1=time+zone, 2=dst, 3(+)=garbage that confuses ServiceStateTracker
+            nitz = nitzParts[0] + "," + nitzParts[1] + "," + nitzParts[2];
+            p.setDataPosition(dataPosition);
+            p.writeString(nitz);
+            p.writeLong(nitzReceiveTime);
+            // The string is shorter now, drop the extra bytes
+            p.setDataSize(p.dataPosition());
+        }
+    }
+
     @Override
     protected Object
     responseSignalStrength(Parcel p) {
@@ -432,6 +450,11 @@ public class SlteRIL extends RIL {
         }
 
         switch (newResponse) {
+            case RIL_UNSOL_NITZ_TIME_RECEIVED:
+                fixNitz(p);
+                p.setDataPosition(dataPosition);
+                super.processUnsolicited(p, type);
+                break;
             case RIL_UNSOL_AM:
                 ret = responseString(p);
                 break;
