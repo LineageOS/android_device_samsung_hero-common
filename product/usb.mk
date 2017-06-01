e8f1948a931b8fc89b3ba316a56aa890c38f6214
Davis Mosenkovs <davikovs@gmail.com>
2017-06-01 14:32:09 -0700

    Disable unauthenticated ADB/fix SafetyNet
    Adjust default.prop to disable unauthenticated ADB during boot.
    This fixes Google SafetyNet (on non-rooted devices).
    
    Change-Id: I0a05bff6701e9e4a9cffa035758bb2faccc5c5b1

--- product/usb.mk
+++ product/usb.mk
@@ -9,10 +9,9 @@ PRODUCT_PACKAGES += \
 # Properties
 # Default.prop overrides to get adb working at boot
 ADDITIONAL_DEFAULT_PROPERTIES += \
-	ro.secure=0 \
-	ro.adb.secure=0 \
+	ro.secure=1 \
+	ro.adb.secure=1 \
 	ro.hardware=universal8890 \
-	persist.service.adb.enable=1 \
 	persist.service.debuggable=1 \
-	persist.sys.usb.config=adb \
+	persist.sys.usb.config=none \
 	ro.securestorage.support=false
