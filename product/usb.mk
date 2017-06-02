4b9da8351b4df132ea50ff65c2e3d9b08878d7cb
Jesse Chan <jc@lineageos.org>
2017-06-02 08:28:02 +0800

    hero: disable insecure adb
    Change-Id: I96a9997f7c13b12c6dcc9be90cd5114687e37f68

--- product/usb.mk
+++ product/usb.mk
@@ -5,14 +5,3 @@ PRODUCT_PACKAGES += \
 # Packages
 PRODUCT_PACKAGES += \
 	com.android.future.usb.accessory
-
-# Properties
-# Default.prop overrides to get adb working at boot
-ADDITIONAL_DEFAULT_PROPERTIES += \
-	ro.secure=0 \
-	ro.adb.secure=0 \
-	ro.hardware=universal8890 \
-	persist.service.adb.enable=1 \
-	persist.service.debuggable=1 \
-	persist.sys.usb.config=adb \
-	ro.securestorage.support=false
