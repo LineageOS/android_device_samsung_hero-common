a8a8a56086405359fa8595f2f18540df3a4ce819
Davis Mosenkovs <davikovs@gmail.com>
2018-01-24 03:34:16 +0000

    Revert "Updated Bluetooth config"
    This reverts commit f3b89effa00964625be7925523c32301b2f11de0.
    
    Change-Id: Icd3ed1b2cde4e5b21018f2cb87bcdec25614f44b

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -1,7 +1,6 @@
 /*
  * Copyright (C) 2012 The Android Open Source Project
  * Copyright (C) 2014 The CyanogenMod Project <http://www.cyanogenmod.org>
- * Copyright (C) 2018 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,7 +18,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Samsung Galaxy S7"
+#define BTM_DEF_LOCAL_NAME   "Samsung Galaxy S7 Edge"
 
 #define BTM_WBS_INCLUDED TRUE       /* Enable WBS */
 #define BTIF_HF_WBS_PREFERRED TRUE  /* Use WBS    */
